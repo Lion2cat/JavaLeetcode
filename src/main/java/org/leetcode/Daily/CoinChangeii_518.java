42299e6a722fcf50a2425eda45235bf9fe1a53c5
Lion2cat <zhaohoran01@gmail.com>
2024-03-25 22:23:15 +0000

    2024.0325 Coins change : 递归搜索 + 保存计算结果 = 记忆化搜索

---
+++ src/main/java/org/leetcode/Daily/CoinChangeii_518.java
@@ -0,0 +1,27 @@
+package org.leetcode.Daily;
+
+import java.util.Arrays;
+
+public class CoinChangeii_518 {
+    public static  class Solution {
+        private int[] coins;
+        private int[][] memo;
+
+        public int change(int amount,int[] coins) {
+            this.coins = coins;
+            int n = coins.length;
+            memo = new int[n][amount + 1];
+            for (int[] row : memo)
+                Arrays.fill(row, -1);
+            int ans = dfs(n - 1, amount);
+            return ans;
+        }
+
+        private int dfs(int i, int c) {
+            if (i < 0) return c == 0 ? 1:0; // 防止下面 + 1 溢出
+            if (memo[i][c] != -1) return memo[i][c];
+            if (c < coins[i]) return memo[i][c] = dfs(i - 1, c);
+            return memo[i][c] = dfs(i - 1, c) + dfs(i, c - coins[i]);
+        }
+    }
+}
